326b1f782bfe6eb825daa483d8edb8f642c3dbd0
MarcoBroto <msoto9940@gmail.com>
2020-09-10 01:18:41 -0600

    Added cd builtin functionality

--- bombshell.py
+++ bombshell.py
@@ -1,15 +1,26 @@
 #! python3.8
-import sys, os, re
+import os, sys, re
 
 BOMBA = chr(0x1F4A3)
 
-def promptCommand() -> str:
-	print(BOMBA, end='')
-	commStr = input()
+def changeDirectory(pathStr):
+	try: os.chdir(pathStr)
+	except FileNotFoundError: print('Directory does not exist.')
+	except PermissionError: print('User does not have permission to access this directory.')
+	except NotADirectoryError: print('Path is not a directory.')
+
+def promptCommand():
+	print(f'{os.getcwd()} {BOMBA}', end=' ')
+	try: commStr = input()
+	except KeyboardInterrupt:
+		print()
+		exit()
 	return commStr
 
 def parseCommand(commStr: str):
-	commStr = re.split('[ ]+', commStr)
+	args = re.split('[ ]+', commStr)
+	if not len(args): return
+	return args[0], args
 
 def runCommand(commStr: str, args):
 	try:
@@ -19,14 +30,26 @@ def runCommand(commStr: str, args):
 		return
 
 	if rc == 0: # Child branch
-		print('Child running.')
-		os.execvp(commStr, args)
+		# print('Child running...')
+		try: os.execvp(commStr, args)
+		except FileNotFoundError:
+			print(f'bombshell: {commStr}: Command not found')
+		# print('Child done running.')
+		exit()
 	elif rc > 0: # Parent branch
-		print('Parent waiting...')
+		# print('Parent waiting...')
 		os.wait()
-		print('Parent done waiting.')
-	else: # Fork error
-		raise OSError
+		# print('Parent done waiting.')
+	else: raise OSError # Fork error
+
+def redirectOutput(writeData):
+	pass
+
+def runInBackground(command, args):
+	pass
 
 if __name__ == "__main__":
-	parseCommand(input())
+	while True:
+		commStr = promptCommand()
+		command, args = parseCommand(commStr)
+		runCommand(commStr, args)
