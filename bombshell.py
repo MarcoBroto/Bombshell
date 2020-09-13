f305a022b10e203ed1a0ce0300e8adfc5b9c7f3b
MarcoBroto <msoto9940@gmail.com>
2020-09-13 00:07:49 -0600

    Update bombshell.py
    + Updated regex when parsing commands to recognize redirected output commands
    + Includes built-in function lookup
    + exit() calls are now sys.exit() calls
    
    Needs pipeline recognition and pass through logic, redirected output writing, and background process integration.

--- bombshell.py
+++ bombshell.py
@@ -2,6 +2,7 @@
 import os, sys, re
 
 BOMBA = chr(0x1F4A3)
+outList = sys.stdout
 
 def changeDirectory(pathStr):
 	try: os.chdir(pathStr)
@@ -9,20 +10,33 @@ def changeDirectory(pathStr):
 	except PermissionError: print('User does not have permission to access this directory.')
 	except NotADirectoryError: print('Path is not a directory.')
 
+BUILT_INS = {'cd': changeDirectory, 'exit': sys.exit}
+
+def redirectOutput(path):
+	os.set_inheritable(1, True)
+	os.open(outList, os.O_CREAT | os.O_WRONLY)
+
 def promptCommand():
 	print(f'{os.getcwd()} {BOMBA}', end=' ')
 	try: commStr = input()
 	except KeyboardInterrupt:
 		print()
-		exit()
+		sys.exit()
 	return commStr
 
 def parseCommand(commStr: str):
-	args = re.split('[ ]+', commStr)
+	outputs = list(map(lambda x: re.sub('> *', '', x.strip()),
+                    re.findall('> *[^ ]+', commStr))) # Parse and store redirected outputs
+	args = re.split('[ ]+', re.sub('> *[^ ]+', '', commStr)) # Parse program arguments
 	if not len(args): return
-	return args[0], args
+	return args
 
-def runCommand(commStr: str, args):
+def runCommand(args):
+	if commStr in BUILT_INS:
+		if len(args): BUILT_INS[commStr](args[1])
+		else: BUILT_INS[commStr]()
+		return
+
 	try:
 		rc = os.fork()
 	except OSError as err:
@@ -31,25 +45,29 @@ def runCommand(commStr: str, args):
 
 	if rc == 0: # Child branch
 		# print('Child running...')
-		try: os.execvp(commStr, args)
-		except FileNotFoundError:
-			print(f'bombshell: {commStr}: Command not found')
+		try:
+			# redirectOutput(outList)
+			print(args)
+			os.execvp(commStr, args)
+			# os.write(1, sys.stdout)
+		except FileNotFoundError: print(f'bombshell: {commStr}: Command not found')
+		except ValueError as err: print(err)
 		# print('Child done running.')
-		exit()
+		sys.exit()
 	elif rc > 0: # Parent branch
 		# print('Parent waiting...')
 		os.wait()
 		# print('Parent done waiting.')
 	else: raise OSError # Fork error
 
-def redirectOutput(writeData):
+def runInBackground(command, args):
 	pass
 
-def runInBackground(command, args):
+def pipeItUp():
 	pass
 
 if __name__ == "__main__":
 	while True:
 		commStr = promptCommand()
-		command, args = parseCommand(commStr)
-		runCommand(commStr, args)
+		args = parseCommand(commStr)
+		runCommand(args)
