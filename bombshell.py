b03a9a75cf018f252e55aba9ff068f6d1402762c
MarcoBroto <msoto9940@gmail.com>
2020-10-08 19:20:16 -0600

    Fixed redirect output and first pipe, resembles Bash behavior
    * Still needs fix to handle multiple pipes
    * Still needs run in background feature

--- bombshell.py
+++ bombshell.py
@@ -3,28 +3,20 @@ import os, sys, re
 
 BOMBA = chr(0x1F4A3) # Bomb character constant
 outList = [] # Used to store names and descriptors of redirect output files
-fd_stdout = None # Used to store copied std file descriptors when using pipe
-
-class MultiOut(object):
-    def __init__(self, *args):
-        self.handles = args
-
-    def write(self, s):
-        for f in self.handles: f.write(s)
+std_fds = None # Used to store copied std file descriptors when using pipe
 
 
 def restoreStdFds():
-	global std_fds
-	os.dup2(0, std_fds[0]) # reopen stdin
-	os.dup2(1, std_fds[1]) # reopen stdout
-	os.set_inheritable(0, True)
-	os.set_inheritable(1, True)
+	if not std_fds: return
+	for i in range(3): # reopen std file descriptors
+		os.dup2(std_fds[i], i)
+		os.set_inheritable(i, True)
 
 
 def changeDirectory(args):
 	if len(args) < 2: args.append(re.search('.{1}', os.getcwd()).group())
 	try: os.chdir(args[1])
-	except FileNotFoundError: print(f'cd: no such file or directory: {args[1]}')
+	except FileNotFoundError: print(f'asdfacd: no such file or directory: {args[1]}')
 	except PermissionError: print('User does not have permission to access this directory.')
 	except NotADirectoryError: print('Path is not a directory.')
 
@@ -40,6 +32,7 @@ def redirectOutput():
 		for line in sys.stdin:
 			for fd in outList: os.write(fd, line.encode())
 		for fd in outList: os.close(fd)
+		sys.stdout.flush()
 	except OSError as err: print(err)
 
 
@@ -52,6 +45,7 @@ def pipeItUp():
 def promptCommand():
 	print(f'{os.getcwd()} {BOMBA}', end=' ')
 	try: commStr = input()
+	# except EOFError: sys.exit(1)
 	except KeyboardInterrupt:
 		print()
 		sys.exit()
@@ -59,10 +53,11 @@ def promptCommand():
 
 
 def parseCommand(commStr: str):
+	sys.stdout.flush()
 	global outList
 	outList = list(map(lambda s: re.sub('> *', '', s.strip()),
                     re.findall('> *[^ ]+', commStr))) # Parse and store redirected outputs
-	args = re.split('[ ]+', re.sub('> *[^ ]+', '', commStr)) # Parse program arguments
+	args = re.split('[ ]+', re.sub('> *[^ ]+', '', commStr).strip()) # Parse program arguments
 	if not len(args): return
 	return args
 
@@ -73,6 +68,8 @@ BUILT_INS = {'cd': changeDirectory, 'exit': sys.exit}
 def runCommand(args: [str], pipe: tuple):
 	# print(f'{args=}')
 	# print(f'{outList=}')
+	# print(f'{pipe=}')
+	
 	if args[0] in BUILT_INS:
 		BUILT_INS[args[0]](args if args[0] != 'exit' else None)
 		return
@@ -82,31 +79,33 @@ def runCommand(args: [str], pipe: tuple):
 		print(f'{err}: Unable to fork')
 		sys.exit(1)
 
+	if pipe: # Route output through pipe
+		os.dup2(pipe[1], 1) # Assign pipe's file descriptor to stdout's
+		os.set_inheritable(1, True)
+		os.close(pipe[1])
+		# for fd in pipe: os.close(fd)
+
 	if rc == 0: # Child branch
 		try:
-			if pipe:
-				os.close(1) # Close stdout
-				os.dup2(pipe[1], 1) # Duplicate pipe's write file descriptor
-				os.set_inheritable(1, True)
-				for fd in pipe: os.close(fd)
-			elif fd_stdout:
-				os.dup2(0, std_fds[0]) # reopen stdin
-				os.dup2(1, std_fds[1]) # reopen stdout
-				for fd in std_fds: os.close(fd)
-			os.execvp(args[0], args)
-		except FileNotFoundError: print(f'bombshell: {commStr}: Command not found')
-		except ValueError as err: print('asdfasdf')
+			# if pipe: # Route output through pipe
+			# 	os.dup2(pipe[1], 1) # Assign pipe's file descriptor to stdout's
+			# 	os.set_inheritable(1, True)
+			# 	for fd in pipe: os.close(fd)
+			if outList: os.dup2(os.open(outList[-1], os.O_CREAT | os.O_WRONLY | os.O_TRUNC), 1) # Redirect output
+			os.execvp(args[0], args) # Execute program
+		except FileNotFoundError: print(f'bombshell: {args[0]}: Command not found')
 		sys.exit()
 	elif rc > 0: # Parent branch
 		os.wait()
+		global std_fds
 		if pipe:
 			os.dup2(pipe[0], 0) # Close stdin, duplicate pipe read fd into stdin fd
 			os.set_inheritable(0, True)
 			for fd in pipe: os.close(fd) # Close parent pipe file descriptors
-		elif fd_stdout:
+		elif std_fds:
 			restoreStdFds() # Reopen closed std file descriptors
-			for fd in std_fds: os.close(fd)
-		redirectOutput() # Redirect output if possible
+			for fd in std_fds: os.close(fd) # Close duplicated std file descriptors
+			std_fds = None
 	else: raise OSError # Fork error
 
 
@@ -114,10 +113,10 @@ if __name__ == "__main__":
 	while True:
 		inputStr = promptCommand()
 		commandList = re.split('[|]', inputStr) # Parse command pipes
-		# print(f'{commandList=}')
 		if len(commandList) > 1: std_fds = os.dup(0), os.dup(1), os.dup(2)  # Duplicate std file descriptors; will be reopened when pipe ends
+		# print(f'{commandList=}')
 
 		while commandList:
-			args = parseCommand(commandList.pop(0).strip())
+			args = parseCommand(commandList.pop(0))
 			pfds = pipeItUp() if len(commandList) >= 1 else None
 			runCommand(args, pipe=pfds)
