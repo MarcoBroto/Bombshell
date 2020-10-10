6a341f8e9fe349e5730d175464c7108ee02e0752
MarcoBroto <msoto9940@gmail.com>
2020-10-10 16:52:23 -0600

    Fixed redirect and piping bugs. Still has stdin bug for user input
    * Bug exists when reading user input from stdin. Blank lines close stdin file descriptor.

--- bombshell.py
+++ bombshell.py
@@ -2,7 +2,7 @@
 import os, sys, re
 
 BOMBA = chr(0x1F4A3) # Bomb character constant
-outList = [] # Used to store names and descriptors of redirect output files
+inList, outList = [], [] # Used to store names and descriptors of redirect input and output files
 std_fds = None # Used to store copied std file descriptors when using pipe
 
 
@@ -14,7 +14,8 @@ def restoreStdFds():
 
 
 def changeDirectory(args):
-	if len(args) < 2: args.append(re.search('.{1}', os.getcwd()).group())
+	if len(args) < 2: args.append(os.environ['HOME'])
+	else: args[1] = re.sub('^~', os.environ['HOME'], args[1]) # Replace tilde with home alias
 	try: os.chdir(args[1])
 	except FileNotFoundError: print(f'asdfacd: no such file or directory: {args[1]}')
 	except PermissionError: print('User does not have permission to access this directory.')
@@ -27,12 +28,10 @@ def runInBackground(command, args): pass #TODO: Implement run program in backgro
 def redirectOutput():
 	global outList
 	try:
-		if not outList: return #TODO: fix bug that removes stdin data when read
-		outList = [os.open(file, os.O_CREAT | os.O_WRONLY) for file in outList] # Store open file descriptors
-		for line in sys.stdin:
-			for fd in outList: os.write(fd, line.encode())
+		if not outList: return
+		outList = [os.open(file, os.O_CREAT | os.O_WRONLY | os.O_TRUNC) for file in outList] # Store open file descriptors
+		for line in sys.stdin: map(lambda fd: os.write(fd, line.encode()), outList)
 		for fd in outList: os.close(fd)
-		sys.stdout.flush()
 	except OSError as err: print(err)
 
 
@@ -43,21 +42,24 @@ def pipeItUp():
 
 
 def promptCommand():
-	print(f'{os.getcwd()} {BOMBA}', end=' ')
-	try: commStr = input()
-	# except EOFError: sys.exit(1)
+	promptStr = re.sub(f"^{os.environ['HOME']}", '~', f'{os.getcwd()} {BOMBA} ')
+	os.write(1, promptStr.encode()) # Print current working directory and prompt symbol
+	try:
+		inputStr = sys.stdin.readline()
+		if not inputStr: sys.exit()
 	except KeyboardInterrupt:
 		print()
 		sys.exit()
-	return commStr
+	return inputStr.strip()
 
 
 def parseCommand(commStr: str):
-	sys.stdout.flush()
-	global outList
+	global inList, outList
+	inList = list(map(lambda s: re.sub('< *', '', s.strip()),
+                    re.findall('< *[^ ]+', commStr))) # Parse and store redirected inputs
 	outList = list(map(lambda s: re.sub('> *', '', s.strip()),
                     re.findall('> *[^ ]+', commStr))) # Parse and store redirected outputs
-	args = re.split('[ ]+', re.sub('> *[^ ]+', '', commStr).strip()) # Parse program arguments
+	args = re.split('[ ]+', re.sub('(> *[^ ]+)|(< *[^ ]+)', '', commStr).strip()) # Parse program arguments
 	if not len(args): return
 	return args
 
@@ -65,32 +67,19 @@ def parseCommand(commStr: str):
 BUILT_INS = {'cd': changeDirectory, 'exit': sys.exit}
 
 
-def runCommand(args: [str], pipe: tuple):
-	# print(f'{args=}')
-	# print(f'{outList=}')
-	# print(f'{pipe=}')
-	
-	if args[0] in BUILT_INS:
+def runCommand(args: [str], pipe: tuple=None):
+	if args[0] in BUILT_INS: # Run built in commands
 		BUILT_INS[args[0]](args if args[0] != 'exit' else None)
 		return
 
-	try: rc = os.fork()
-	except OSError as err:
-		print(f'{err}: Unable to fork')
-		sys.exit(1)
-
-	if pipe: # Route output through pipe
-		os.dup2(pipe[1], 1) # Assign pipe's file descriptor to stdout's
-		os.set_inheritable(1, True)
-		os.close(pipe[1])
-		# for fd in pipe: os.close(fd)
-
+	rc = os.fork()
 	if rc == 0: # Child branch
 		try:
-			# if pipe: # Route output through pipe
-			# 	os.dup2(pipe[1], 1) # Assign pipe's file descriptor to stdout's
-			# 	os.set_inheritable(1, True)
-			# 	for fd in pipe: os.close(fd)
+			if inList: os.dup2(os.open(inList[-1], os.O_RDONLY), 0) # Redirect input
+			if pipe: # Route output through pipe
+				os.dup2(pipe[1], 1) # Assign pipe's file descriptor to stdout's
+				os.set_inheritable(1, True)
+				for fd in pipe: os.close(fd)
 			if outList: os.dup2(os.open(outList[-1], os.O_CREAT | os.O_WRONLY | os.O_TRUNC), 1) # Redirect output
 			os.execvp(args[0], args) # Execute program
 		except FileNotFoundError: print(f'bombshell: {args[0]}: Command not found')
@@ -106,17 +95,23 @@ def runCommand(args: [str], pipe: tuple):
 			restoreStdFds() # Reopen closed std file descriptors
 			for fd in std_fds: os.close(fd) # Close duplicated std file descriptors
 			std_fds = None
-	else: raise OSError # Fork error
+	else: raise OSError('Fork Error') # Fork error
 
 
 if __name__ == "__main__":
 	while True:
 		inputStr = promptCommand()
-		commandList = re.split('[|]', inputStr) # Parse command pipes
-		if len(commandList) > 1: std_fds = os.dup(0), os.dup(1), os.dup(2)  # Duplicate std file descriptors; will be reopened when pipe ends
-		# print(f'{commandList=}')
+		# print(f'{inputStr=}')
+		if not inputStr: continue
+		pipeStream = re.split('[|]', inputStr) # Parse command pipes
+		if len(pipeStream) > 1: std_fds = os.dup(0), os.dup(1), os.dup(2)  # Duplicate std file descriptors; will be reopened when pipe ends
+		# print(f'{pipeStream=}')
+		if len(pipeStream) <= 0: continue
 
-		while commandList:
-			args = parseCommand(commandList.pop(0))
-			pfds = pipeItUp() if len(commandList) >= 1 else None
-			runCommand(args, pipe=pfds)
+		while pipeStream:
+			args = parseCommand(pipeStream.pop(0))
+			pfds = pipeItUp() if len(pipeStream) >= 1 else None # Create pipe file descriptors
+			try: runCommand(args, pipe=pfds)
+			except OSError as err:
+				os.write(2, f'{err}\n'.encode())
+				sys.exit(1)
